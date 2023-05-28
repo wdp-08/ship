04e1ba59c7d52e904c445055ad2c03f8e0c553cf
gemagheina19 <gheina.gemalia@ui.ac.id>
2023-05-28 19:31:12 +0700

    output 2

--- stars.js
+++ stars.js
@@ -7,11 +7,20 @@ function createStar1() {
 let star = '';
 for (let i = 0; i < row; i++) {
 star += symbol.repeat(i + 0) + '\n';
+
 }
 
 for (let j = 1; j < row; j++) {
 star += symbol.repeat(j + 2) + '\n';
 }
+
+for (let i = row + 0; i >= 2; i--) {
+    star += symbol.repeat(i + 6) + '\n';
+  }
+
+  for (let j = row + 0; j >= 2; j--) {
+    star += symbol.repeat(j + 3) + '\n';
+  }
 
 return star;
 }
